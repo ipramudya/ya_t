92980a640cb474fc53f19cdac65460616ada9189
ipramudya <ipramudya.dev@gmail.com>
2024-11-24 07:04:27 +0700

    add dynamic domain url

--- api/client.ts
+++ api/client.ts
@@ -1,9 +1,8 @@
+import { DOMAIN_URL } from "@/constant";
 import { AuthService } from "./services/authService";
 import { ProfileService } from "./services/profileService";
 
-const NEXT_API_URL = "http://localhost:3000";
-
 export const api = {
-    authService: new AuthService({ baseURL: NEXT_API_URL }),
-    profileService: new ProfileService({ baseURL: NEXT_API_URL, useAuth: true })
+    authService: new AuthService({ baseURL: DOMAIN_URL }),
+    profileService: new ProfileService({ baseURL: DOMAIN_URL, useAuth: true })
 };
--- constant/index.ts
+++ constant/index.ts
@@ -3,3 +3,7 @@ export const DATABASE_URL =
 export const CLOUDINARY_CLOUD_NAME = "dn4pc5cov";
 export const CLOUDINARY_API_KEY = "688456573383585";
 export const CLOUDINARY_API_SECRET = "Cnn7ZD1PXtRE-CyEag-1paz6sfw";
+export const DOMAIN_URL =
+    process.env.NODE_ENV === "production"
+        ? "https://youapp-pram.vercel.app"
+        : "http://localhost:3000";
